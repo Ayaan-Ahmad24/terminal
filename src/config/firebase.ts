84f488b1bd36d0fa12a102c9561aa8a4fbfa667d
Ayaan Ahmad <ayaansheikh061@gmail.com>
2025-01-25 03:22:18 +0500

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC7ltaIOO-ZxNf65KX4Cb4uFxGpsHU-pD8",
-  authDomain: "https://pdf-app-65a58.firebaseapp.com/",
-  projectId: "pdf-app-65a58",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
   // storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "1094167268194",
-  appId: "1:1094167268194:android:6219209b6549c71ef7fa3b"
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
